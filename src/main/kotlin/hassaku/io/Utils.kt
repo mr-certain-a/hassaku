4f8da116618793554cf6992545b58cfeee3b8a62
mr-certain-a <mr.certain.a@gmail.com>
2020-10-14 17:54:58 +0900

    added the io functions

--- src/main/kotlin/hassaku/io/Utils.kt
+++ src/main/kotlin/hassaku/io/Utils.kt
@@ -1,8 +1,32 @@
 package hassaku.io
 
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.stream.Stream
+
+fun Path.createDirectories(): Path {
+    return Files.createDirectories(this)
+}
+
+fun Path.list(): Stream<Path> {
+    return Files.list(this)
+}
+
+fun Path.delete() {
+    Files.delete(this)
+}
 
 fun String.toPath(): Path {
     return Paths.get(this)
 }
+
+val Path.isDirectory: Boolean
+    get() = Files.isDirectory(this)
+
+val Path.nameWithoutExtension: String
+    get() = this.toFile().nameWithoutExtension
+
+val Path.extension: String
+    get() = this.toFile().extension
+
